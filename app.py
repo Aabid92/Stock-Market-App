b902be66bdf886eab93167314f4b4487857f7421
Aabid92 <shaikhaabid608@gmail.com>
2022-05-11 01:25:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ with st.sidebar:
                         menu_icon="app-indicator", default_index=0,
                         styles={
         "container": {"padding": "5!important", "background-color": "F2F2F2"},
-        "nav-link": {"font-size": "12px", "text-align": "left", "margin":"0px", "--hover-color": "#ECECEC"},
+        "nav-link": {"font-size": "12px", "text-align": "left", "margin":"0px", "--hover-color": "#203239"},
         "nav-link-selected": {"background-color": "#2e54ff"}, ##2e54ff
     }
     )
