f28c8cc92f75981e54c51fcaeab6dc38a6b692d9
Aabid92 <shaikhaabid608@gmail.com>
2022-06-02 16:59:55 +0530

    removing default menu

--- app.py
+++ app.py
@@ -22,7 +22,13 @@ st.set_page_config(page_title="Stock Market Analysis", page_icon= image1)
 image = Image.open('main.png')
 st.image(image)
 
+hide_menu_style = """
+                <style>
+                #MainMenu {visibility: hidden; footer {visibility: hidden;}}
+                </style>
 
+                """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 
 
