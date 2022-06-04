628a518022e3359eaae4ecbb0a2937161a4c65a5
Aabid92 <shaikhaabid608@gmail.com>
2022-06-04 10:46:49 +0530

    adding html

--- pages/Mutual Funds.py
+++ pages/Mutual Funds.py
@@ -15,7 +15,13 @@ import numpy as np
 from PIL import Image
 import cufflinks as cf
 
+hide_menu_style = """
+                <style>
+                #MainMenu {visibility: hidden; footer {visibility: hidden;}}
+                </style>
 
+                """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 
 def mutual_fund():
--- pages/Stock Analysis.py
+++ pages/Stock Analysis.py
@@ -16,7 +16,13 @@ from PIL import Image
 import cufflinks as cf
 
 
+hide_menu_style = """
+                <style>
+                #MainMenu {visibility: hidden; footer {visibility: hidden;}}
+                </style>
 
+                """
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 def technical():
         
